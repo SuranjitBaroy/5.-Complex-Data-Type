9da2adb941a03582e13edd8ad0f7ae484e521747
Suranjit Baroy <62200368+SuranjitBaroy@users.noreply.github.com>
2021-06-17 15:16:15 +0600

    Add files via upload
    Complex Data Type

---
+++ learn.js
@@ -0,0 +1,51 @@
+/*Complex Data Type:
+
+1. Object 2.Array 3. Null . Function
+
+*/
+
+
+//1. Object
+
+
+var obj = {
+        
+        name  : "Nokia",
+        price : 999
+};
+
+document.write(typeof(obj));
+
+console.log(obj);
+
+//array 
+var array = [1,2,3,4];
+
+console.log(array);
+
+console.log(typeof(array));
+
+//Null
+
+
+var zero = null;
+
+
+console.log(typeof(null));
+
+
+
+//Function 
+
+let TheDate = new Date();
+
+
+console.log(TheDate);
+
+console.log(typeof(TheDate));
+
+
+
+
+
+
